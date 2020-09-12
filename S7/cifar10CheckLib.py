75ff0703368a57f5fa04bed4cc0a853c431a7149
rashidmeras <42776940+rashidmeras@users.noreply.github.com>
2020-09-13 01:15:35 +0530

    Create cifar10CheckLib.py

---
+++ S7/cifar10CheckLib.py
@@ -0,0 +1,38 @@
+def check_predict(net):
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+    
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+    
+    
+def check_test_accuracy(net, device):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device) , data[1].to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+    
+    
+def check_classwise_accuracy(net, device):
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device) , data[1].to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+    
+    
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i])) 
