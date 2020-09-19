fd73916334d572aa0868e3d6ceab681e75620f36
rashidmeras <42776940+rashidmeras@users.noreply.github.com>
2020-09-19 06:50:44 +0530

    Update initialize_train_test_loader_cifar10.py

--- S7/initialize_train_test_loader_cifar10.py
+++ S7/initialize_train_test_loader_cifar10.py
@@ -17,17 +17,17 @@ def train_test_loader(batch_size = 4, num_workers=2):
                                   )
     
     train_transform = transforms.Compose([
-                            transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),
-                            transforms.RandomAffine(degrees=10, translate=(0.1,0.1), scale=(0.9, 1.1)),
-                            transforms.RandomRotation(15),                          
+                            transforms.ColorJitter(brightness=0.10, contrast=0.1, saturation=0.10, hue=0.1),        
+                            transforms.RandomHorizontalFlip(p=0.5),
+                            transforms.RandomCrop(32, padding=2),                  
                             transforms.ToTensor(),
                             transforms.Normalize(mean, std)
-                        ])
+                           ])
+    
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     
     return train_loader, test_loader
